3453b595c8a3e0b30531912ddbaae5358b1c9dce
Sahand Edrisi <107874587+Sahand-Edrisi@users.noreply.github.com>
2025-10-24 16:38:52 +0330

    add dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@ import BackGroundImage from "@/components/BackGroundImage";
 import SocialMedia from "@/components/socialMedia/SocialMedia";
 import AboutMe from "@/components/aboutMe/AboutMe";
 import Skills from "@/components/skills/Skills";
-import Experience from "@/components/experience/Experience";
-import Projects from "@/components/projects/Projects";
-import ContactMe from "@/components/contactMe/ContactMe";
+import dynamic from "next/dynamic";
+
+const Projects = dynamic(() => import("@/components/projects/Projects"));
+
+const Experience = dynamic(() => import("@/components/experience/Experience"));
+
+const ContactMe = dynamic(() => import("@/components/contactMe/ContactMe"));
 
 export default function Home() {
   return (
-    <div className="max-w-[1444px] m-auto border-l-4 border-green border-r-4   border-b-4  ">
+    <div className="max-w-[1444px] m-auto border-l-4 border-green border-r-4 border-b-4">
       <BackGroundImage />
       <SocialMedia />
       <AboutMe />
