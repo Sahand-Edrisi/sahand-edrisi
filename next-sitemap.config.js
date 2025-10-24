bb8d74dd762e105d3b02f0b69e6fd73cf5c69c9e
Sahand Edrisi <107874587+Sahand-Edrisi@users.noreply.github.com>
2025-10-24 16:23:37 +0330

    remove /

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://sahand-edrisi.ir/', 
+  siteUrl: 'https://sahand-edrisi.ir', 
   generateRobotsTxt: true, 
   sitemapSize: 7000,
   changefreq: 'daily',
