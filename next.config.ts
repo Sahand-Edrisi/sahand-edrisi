f75abf9338626c0644c9128bccc368d9533cbfd4
Sahand Edrisi <107874587+Sahand-Edrisi@users.noreply.github.com>
2025-10-25 12:45:47 +0330

    remove

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/sahand-edrisi",
-  assetPrefix: "/sahand-edrisi/",
   images: {
     unoptimized: true,
   },
