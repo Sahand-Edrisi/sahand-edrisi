5e317e05f562a9d407a7a2f3eeaec8e923f701c9
Sahand Edrisi <107874587+Sahand-Edrisi@users.noreply.github.com>
2025-10-25 12:44:38 +0330

    basePath & assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
+  basePath: "/sahand-edrisi",
+  assetPrefix: "/sahand-edrisi/",
   images: {
-    unoptimized: true, 
+    unoptimized: true,
   },
 };
 
