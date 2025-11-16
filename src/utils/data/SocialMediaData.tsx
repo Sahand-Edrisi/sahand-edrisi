8cc12c1714ef4b7521e36bb1c4f7619df1f4c7d1
Sahand Edrisi <107874587+Sahand-Edrisi@users.noreply.github.com>
2025-11-16 16:21:55 +0330

    Remove Twitter

--- src/utils/data/SocialMediaData.tsx
+++ src/utils/data/SocialMediaData.tsx
@@ -2,37 +2,31 @@ import Linkedin from "../../../public/icons/linkedin.png";
 import Github from "../../../public/icons/github.png";
 import CVIcon from "../../../public/icons/cv.png";
 import WhatsApp from "../../../public/icons/social.png";
-import Twitter from "../../../public/icons/twitter.png";
 import Telegram from "../../../public/icons/telegram.png";
 
 export const socialMedia = [
   {
     id: 1,
-    src: Twitter,
-    href: "https://x.com/sahand_edrisi",
-  },
-  {
-    id: 2,
     src: Telegram,
     href: "https://t.me/Sahand_edc",
   },
   {
-    id: 3,
+    id: 2,
     src: WhatsApp,
     href: "https://wa.me/989195812500",
   },
   {
-    id: 4,
+    id: 3,
     src: Linkedin,
     href: "https://www.linkedin.com/in/sahand-edrisi/",
   },
   {
-    id: 5,
+    id: 4,
     src: Github,
     href: "https://github.com/Sahand-Edrisi",
   },
   {
-    id: 6,
+    id: 5,
     src: CVIcon,
     href: "../../../public/CV/Sahand_Edrisi.pdf",
     download: "Sahand_Edrisi.pdf",
